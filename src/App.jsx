bc6bade2ad1790ccfd9fca94c3f1ca424036bd51
alexalves011 <alexalves.work@gmail.com>
2026-03-23 23:54:36 -0300

    Inserindo variáveis de cores, logo e limpeza do projeto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from "./assets/LogoPoupApp.svg";
+
+function App() {
+  return (
+    <>
+      <aside>
+        <img src={logo} alt="" />
+      </aside>
+    </>
+  );
+}
+
+export default App;
