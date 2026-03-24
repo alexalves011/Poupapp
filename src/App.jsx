8e43b6072dac978a2aefe4994eec8c2c3d696cae
alexalves011 <alexalves.work@gmail.com>
2026-03-24 09:41:44 -0300

    Criando Contaoner Principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import logo from "./assets/LogoPoupApp.svg";
+import { Container } from "./components/Container";
 
 function App() {
   return (
-    <>
+    <Container>
       <aside>
         <img src={logo} alt="" />
       </aside>
-    </>
+    </Container>
   );
 }
 
